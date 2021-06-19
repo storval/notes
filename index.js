740f65f4fe8ffe2b5ddf608ba6d62da973d5e03e
Ar on <aronbjarklind@gmail.com>
2021-06-19 19:35:55 +0000

    v 1.0 af síðunni til að halda utan um minnisbók f kóða

---
+++ index.js
@@ -0,0 +1,16 @@
+/*jshint esversion: 6 */ 
+
+let coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
